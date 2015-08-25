6966674d1178abbbad5cc8b5d4d810aa3d1a0cc3
michaleencraig <michaleen@microarts.com>
2015-08-25 15:02:27 -0400

    Initial Commit

---
+++ data/global.php
@@ -0,0 +1,19 @@
+<?php
+$root_dir='';
+$host ='';
+if (isset($_SERVER['HTTP_HOST']) AND (!empty($_SERVER['HTTP_HOST']))) {
+	$host = $_SERVER['HTTP_HOST'];
+}
+if ($host == "localhost") {
+	$root_dir = 'http://localhost/microarts/html';
+} else {
+	$root_dir = 'http://'. $host;
+}
+
+
+$year = @date('Y');
+$current_url  = 'http://'.$host.$_SERVER['REQUEST_URI'] ;
+$styles_dir = $root_dir.'/ux/css/';
+$scripts_dir = $root_dir.'/ux/scripts/';
+$img_dir =  $root_dir.'/content/';
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include ("data/global.php");   ?>
+<!doctype html>
+<html class="no-js" lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="description" content="">
+        <meta property="og:title" content="">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="<?php echo $current_url;?>">
+        <meta property="og:image" content="">
+        <meta property="og:description" content="">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <title>MicroArts Creative Agency</title>
+
+        <link rel="stylesheet" href="<?php echo $styles_dir;?>main.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+
+
+	</head>
+	<body class="home">
+
+
+
+		<header class="primary-header">
+		</header>
+
+		<div class="main" role="main">
+
+		</div>
+
+		<footer class="primary-footer">
+
+		</footer>
+
+	</body>
+</html>
