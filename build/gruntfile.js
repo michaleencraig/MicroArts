c1492ac26e936bc41daceb7aa53c124c3faa5e5b
seancrater <seanc@microarts.com>
2015-08-26 17:23:40 -0400

    Adding Uglify for JS compression

--- build/gruntfile.js
+++ build/gruntfile.js
@@ -3,14 +3,24 @@ module.exports = function( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          '../ux/js/scripts.min.js': ['../ux/js/**/*.js']
+        }
+      }
+    },
     watch: {
-      files: '../ux/sass/**/*.scss',
-      tasks: [ 'sass', 'cssmin', 'minified' ]
+      css: {
+        files: '../ux/sass/**/*.scss',
+        tasks: [ 'sass', 'cssmin' ]
+      },
     },
     sass: require( './custom_modules/sass' ).task,
-    cssmin: require( './custom_modules/cssmin' ).task,
-    minified: require( './custom_modules/minified' ).task
+    cssmin: require( './custom_modules/cssmin' ).task
   });
 };
---
+++ ux/js/scripts.js
@@ -0,0 +1,2 @@
+//Scripts go in here
+//To render a minified version, run cd /Users/seancrater/MicroArts/build then grunt uglify
