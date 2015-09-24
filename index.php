261565989f75447fcaf053592d44f2f70a75f0a0
seancrater <seanc@microarts.com>
2015-09-24 11:57:48 -0400

    Adding basic menu

--- index.php
+++ index.php
@@ -28,7 +28,31 @@
 
 
 		<header class="primary-header">
-            <div class="content"><a href="#" class="logo">MicroArts Creative Agency</a></div>
+            <div class="content">
+                <a href="#" class="logo">MicroArts Creative Agency</a>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Agency</a>
+                        </li>
+                        <li>
+                            <a href="#">Fit</a>
+                        </li>
+                        <li>
+                            <a href="#">Strategies &amp; Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Results</a>
+                        </li>
+                        <li>
+                            <a href="#">Insights</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 		</header>
 
 		<div class="main" role="main">
